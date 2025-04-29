deb047ccdd84aa0e2086205541ee2384227ac9a3
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-28 19:20:36 -0600

    added models to admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recipe, Collection, Comment, Rating
+
+admin.site.register([Recipe, Collection, Comment, Rating])
+
 
 # Register your models here.
