3386ba64ad691309e3320f3e31132244e72f7ca0
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-30 14:58:11 -0600

    made recipe on collection optional when first creating collection

--- main_app/models.py
+++ main_app/models.py
@@ -44,7 +44,7 @@ class Recipe(models.Model):
 class Collection(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="collections")
-    recipes = models.ManyToManyField(Recipe, related_name="collections")
+    recipes = models.ManyToManyField(Recipe, related_name="collections", blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.user.username})"
