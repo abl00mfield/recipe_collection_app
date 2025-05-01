3ef998d324ca9af4a60a6725b7cb7551baa6a738
abl00mfield <amanda.bloomfield@gmail.com>
2025-05-01 11:55:12 -0600

    added pagination to recipe list

--- main_app/views.py
+++ main_app/views.py
@@ -63,7 +63,7 @@ class RecipeList(ListView):
     template_name = "recipes/recipe_list.html"
     context_object_name = "recipes"
     ordering = ["-created_at"]
-    paginate_by = 5
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
