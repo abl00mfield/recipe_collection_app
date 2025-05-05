27ee25e8d9b3a915ade175b4dd16c8d4471185b0
abl00mfield <amanda.bloomfield@gmail.com>
2025-05-05 12:40:02 -0600

    fixed modal for mobile layout

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -42,6 +42,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  //hides the dropdown menu if a user clicks outside of it
+  document.addEventListener("click", function (e) {
+    document.querySelectorAll(".collection-dropdown").forEach((dropdown) => {
+      const isDropdown = dropdown.contains(e.target);
+      const isButton = e.target.closest(".add-icon");
+      if (!isDropdown && !isButton) {
+        dropdown.classList.add("hidden");
+      }
+    });
+  });
+
   if (cancelBtn) {
     cancelBtn.addEventListener("click", () => {
       modal.classList.add("hidden");
